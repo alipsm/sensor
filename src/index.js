9f4634f5ebbe2d6809e01bd706f7b07264f0fabb
storm <stooormix@gmail.com>
2022-04-28 15:09:28 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DashboardContext from './component/contextApi/DashboardContext';
 
-ReactDOM.render(
-    <App />,
+ReactDOM.render(<DashboardContext>
+    <App /></DashboardContext>,
   document.getElementById('root')
 );
 
